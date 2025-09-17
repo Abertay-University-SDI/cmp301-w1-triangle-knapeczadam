6754a1965e98f6c80f47a185b7d47854e8937db4
Ádám Knapecz <knapecz.adam@gmail.com>
2025-09-17 14:40:28 +0100

    Increase the triangle's size (2x)

--- E1_Geometry/E1_Geometry/shaders/colour_vs.hlsl
+++ E1_Geometry/E1_Geometry/shaders/colour_vs.hlsl
@@ -24,12 +24,17 @@ struct OutputType
 OutputType main(InputType input)
 {
 	OutputType output;
+
+	float4x4 scaleMatrix = (float4x4) 0;
+	scaleMatrix._11_22_33 = 2.0f;
+	scaleMatrix._44 = 1.0f;
 	
 	// Change the position vector to be 4 units for proper matrix calculations.
 	input.position.w = 1.0f;
     
 	// Calculate the position of the vertex against the world, view, and projection matrices.
-	output.position = mul(input.position, worldMatrix);
+	output.position = mul(input.position, scaleMatrix);
+	output.position = mul(output.position, worldMatrix);
 	output.position = mul(output.position, viewMatrix);
 	output.position = mul(output.position, projectionMatrix);
 
